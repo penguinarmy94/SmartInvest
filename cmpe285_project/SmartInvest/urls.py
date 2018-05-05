cdfa804ed38fda22f0837f00c5fd88adb1d46022
penguinarmy94 <luisote94@gmail.com>
2018-05-05 14:54:59 -0700

    Added login to SmartInvest

--- cmpe285_project/SmartInvest/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-from . import views
-
-urlpatterns = [
-    path('', views.stock_calculator, name='stock_calculator'),
-    path('result', views.stock_portfolio, name='stock_portfolio'),
-]
