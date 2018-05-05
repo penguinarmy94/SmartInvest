f4e6d5f912c77eb7d3d3d77cfc89b9087d865fbf
penguinarmy94 <luisote94@gmail.com>
2018-05-05 14:41:34 -0700

    Add full project login

---
+++ SmartInvest/myproject/urls.py
@@ -0,0 +1,29 @@
+"""myproject URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/2.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.contrib.auth import views as auth_views
+from django.conf.urls import url
+
+from stockapp import views
+from accounts import views as accounts_views
+
+urlpatterns = [
+    url(r'^$', views.home, name='home'),
+    url(r'^signup/$', accounts_views.signup, name='signup'),
+    url(r'^login/$', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+    url(r'^logout/$', auth_views.LogoutView.as_view(), name='logout'),
+    url(r'^admin/', admin.site.urls),
+]
---
+++ SmartInvest/venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\lw\msse\cmpe-285\project\venv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
