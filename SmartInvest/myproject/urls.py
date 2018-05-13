9cbb2879afce7abfcdd2494bfdfee5205958c702
Arturo Montoya <montoya332@live.com>
2018-05-12 20:52:14 -0700

    home page cards

--- SmartInvest/myproject/urls.py
+++ SmartInvest/myproject/urls.py
@@ -27,7 +27,7 @@ urlpatterns = [
     url(r'^login/$', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
     url(r'^logout/$', auth_views.LogoutView.as_view(), name='logout'),
     url(r'^admin/', admin.site.urls),
-    url(r'^trends/', views.trends, 'trends'),
+    url(r'^trends/', views.trends, name='trends'),
     path('', include('InvestMain.urls')),
     
 ]
--- SmartInvest/stockapp/views.py
+++ SmartInvest/stockapp/views.py
@@ -3,7 +3,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+cards = [
+{'header':'Invest','body':'','src':'/invest'},
+{'header':'Portfolio','body':'','src':'/portfolio'},
+{'header':'Trends','body':'','src':'/trends'},
+{'header':'About','body':'','src':'/about'}
+]
+
+
 def home(request):
-    return render(request, 'home.html')
+    return render(request, 'home.html',{'cards':cards})
 def trends(request):
     return render(request, 'trends.html')
