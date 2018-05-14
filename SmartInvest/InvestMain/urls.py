63117b152f4ef0ffe43e79d24d72eaddf8185805
penguinarmy94 <luisote94@gmail.com>
2018-05-14 04:19:32 -0700

    Modified urls for the Investment side of the app

--- SmartInvest/InvestMain/urls.py
+++ SmartInvest/InvestMain/urls.py
@@ -3,7 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('result', views.stock_portfolio, name='stock_portfolio'),
+    path('portfolio', views.stock_portfolio, name='stock_portfolio'),
     path('invest', views.stock_invest_options, name='invest_view'),
-    path('test', views.test_route, name='test_path'),
 ]
