3d5bfdba9df2bba3b32a2f810448cb22ce705ad4
penguinarmy94 <luisote94@gmail.com>
2018-05-14 04:18:51 -0700

    Added Strategy model for storing stock data for user

--- SmartInvest/InvestMain/models.py
+++ SmartInvest/InvestMain/models.py
@@ -3,6 +3,18 @@ from django.db import models
 class Strategy(models.Model):
 	userid = models.CharField(max_length=100)
 	name = models.CharField(max_length=100)
+	lookup = models.CharField(max_length=100)
 	number_of_stocks_1 = models.IntegerField()
 	number_of_stocks_2 = models.IntegerField()
 	number_of_stocks_3 = models.IntegerField()
+	
+	def __str__(self):
+		string = self.name + "\n"
+		string += ("Share 1: " + str(self.number_of_stocks_1) + "\n")
+		string += ("Share 2: " + str(self.number_of_stocks_2) + "\n")
+		string += ("Share 3: " + str(self.number_of_stocks_3) + "\n")
+		
+		return string
+		
+		
+		
