81491ad9e16e2ccf27a8b2e1a3421297567b887b
penguinarmy94 <luisote94@gmail.com>
2018-05-15 22:12:29 -0700

    Fixed merge issues

--- SmartInvest/stockapp/views.py
+++ SmartInvest/stockapp/views.py
@@ -9,31 +9,6 @@ __loader = json.load(__file)
 linksCards = __loader['linksCards']
 teamsCards = __loader['teamsCards']
 
-<<<<<<< HEAD
-=======
-teamsCards = [
-{
-'title':'Marianne Paulson',
-'backText':' I am a Software Engineer at Lawrence Livermore National Laboratory and love the field of Software Engineering. On my free time I like to hang out with my family and occationally explore my endurance limits on all the beautiful trails California has to offer.',
-'img':'/img/marianne_paulson.jpg'
-},
-{
-'title':'Luis Otero',
-'backText':'I am a Software Engineering student at San Jose State University working on his MS degree. Most of my experience has been in Android development and PHP server-side web development. Worked on the invest and portfolio portion of this project.',
-'img':'/img/luis_otero.jpg'
-},
-{
-'title':'Lalini Wudali',
-'backText':'I work as a Technical Program Manager for an EMR (Electronic Medical Records) project at a Home Health care company. In my free time, I love to spend time with my family and sing Indian classical music',
-'img':'/img/lalini_wudali.jpeg'
-},
-{
-'title':'Arturo Montoya',
-'backText':'I have a bachelor’s degree from SJSU in Computer Engineering. On my down time, I like going on walks with my dog and watching him run around playing with other dogs. I also enjoy challenging myself by learning new things such as juggling (next on my list slack-lining).',
-'img':'/img/arturo_montoya.jpeg'
-}
-]
->>>>>>> d1527936ee8cb87105509734d0fef0b8d61c7866
 def home(request):
     return render(request, 'home.html',{'cards':linksCards})
 def trends(request):
